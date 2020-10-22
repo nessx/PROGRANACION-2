bb7deabdcfe8f10d4725008575e7dc6e825440b6
Nestor Santana <cf17nestor.santana@iesjoandaustria.org>
2020-10-22 13:58:19 +0200

    Update filechooser.java

--- filechooser.java
+++ filechooser.java
@@ -3,7 +3,7 @@ import java.io.File;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileSystemView;
 
-public class FileChooser1 {
+public class filechooser {
 
 	public static void main(String[] args) {
 
