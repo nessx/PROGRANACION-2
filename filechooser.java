15d1318929772e689e0ea1cdbc5cc531ac89fe3d
Nestor Santana <cf17nestor.santana@iesjoandaustria.org>
2020-10-23 10:48:09 +0200

    Update filechooser.java

--- filechooser.java
+++ filechooser.java
@@ -1,13 +1,35 @@
-import java.io.File;
+import java.io.*;
 
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileSystemView;
 
 public class filechooser {
 
-	public static void main(String[] args) {
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent) {
+        JFileChooser filechooser = new JFileChooser();
+        filechooser.showOpenDialog(filechooser);
 
-		JFileChooser file = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        try {
+            String ruta = filechooser.getSelectedFile().getAbsolutePath();
+            BufferedReader br = new BufferedReader(new FileReader (ruta));
+            String line;
+            String texto = "";
+            line.br.readline();
+            while (line != null){
+                texto += line;
+                line = br.readline();
+            }
+
+            //JAVA FX EXAMPLE TEST ONE
+            JFrame ventana = new JFrame("Heloo JAVA");
+            JTextField texto = new JTextField(10); 
+            ventana.setLayout(new FlowLayout());
+            ventana.setSize(450, 400);
+            ventana.setVisible(true);
+            ventana.add(texto);
+            texto.setText(texto),
+		
+		/* JFileChooser file = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
 
 		int returnValue = file.showOpenDialog(null);
 		// int returnValue = jfc.showSaveDialog(null);
@@ -17,6 +39,15 @@ public class filechooser {
 			System.out.println(selectedFile.getAbsolutePath());
 		}
 
-	}
+
+		*/
+        } catch (Exception e) {
+            //TODO: handle exception
+        }  
+    }
+
+    public static void main(String[] args) {
+        jButton1ActionPerformed ba = new jButton1ActionPerformed();
+    }
 
 }
